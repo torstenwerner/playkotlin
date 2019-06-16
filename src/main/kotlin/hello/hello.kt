a31a3a16cf27cbea543e9f96c9416c815e48fb81
Torsten Werner <torsten.werner@westernacher.com>
2019-06-16 18:26:10 +0200

    upgrade gradle and kotlin

--- src/main/kotlin/hello/hello.kt
+++ src/main/kotlin/hello/hello.kt
@@ -1,5 +1,13 @@
 package hello
 
+data class Person(val firstName: String, val lastName: String)
+
+val lazy: String by lazy {
+    "foo" + "bar"
+}
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val person = Person("Hans", "Wurst")
+    println("Hello " + person)
+    println(lazy)
 }
